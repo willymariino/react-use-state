a6d9144af294ee57636690b79fd8801ea158a295
willymariino <wg.mariino@gmail.com>
2025-04-08 19:15:26 +0200

    usestate

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,9 +2,29 @@ import { useState } from "react";
 import Languages from "./languages/languages";
 import Header from "./header";
 function main() {
+    const [selected, setSelected] = useState(languages[0])
+    // selected è la variabile di stato che contiene il linguaggio attualmente selezionato
+    // setSelected è la funzione che aggiorna lo stato
+    // useState(languages[0]) lo stato parte dal primo elemento della lista
     return (
         <div>
             <Header />
+            <div className="container-buttons">
+                {languages.map(lang =>
+                    <button>
+
+
+                    </button>
+
+                )
+
+
+
+                }
+
+
+
+            </div>
 
 
         </div>
